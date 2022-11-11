f1ce129ac6191f2992532f1202d2f1398e8cf2b0
RasmusClausen <rasmusclausen123@gmail.com>
2022-11-11 16:42:59 +0100

    1

--- src/Task3.java
+++ src/Task3.java
@@ -3,7 +3,7 @@ public class Task3 {
     // Create a for loop that prints "10 8 6 4 2"
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 10; i++){
+        for (int i = 10; i > 0; i--){
             if (i % 2 == 0){
                 System.out.println(i);
             }
--- src/Task6.java
+++ src/Task6.java
@@ -1,10 +1,64 @@
 public class Task6 {
-    //    Task 6
-//    Look at this code and try to understand how it works and what it will print to the screen when you
-//    run it.
+    //        Task 6
+//        Look at this code and try to understand how it works and what it will print to the screen when you
+//        run it.
+//
+//        int rows = 4;
+//        int cols = 4;
+//        for (int row = 0; row < rows; row++) {
+//            for (int col = 0; col < cols; col++) {
+//                System.out.print("X");
+//        }
+//            System.out.println();
+//    }
+//
+//      Modify the code in 4 different ways so that you can create the following 4 outputs, and remember
+//      that you can test if a row is even by using this if statement: if (row % 2 == 0)
+//
     public static void main(String[] args) {
 
+//      Task 6.1:
+//
+//      XXXX
+//      OOOO
+//      XXXX
+//      OOOO
 
+        int rows = 4;
+        int cols = 4;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (row % 2 == 0){
+                    System.out.print("X");
+                }
+                else if (row % 2 != 0){
+                    System.out.print("O");
+                }
+            }
+            System.out.println();
+        }
+
+
+//      Task 6.2:
+//
+//      XOXO
+//      XOXO
+//      XOXO
+//      XOXO
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (row % 2 == 0){
+                    System.out.print("X");
+                }
+                else if (row % 2 != 0){
+                    System.out.print("O");
+                }
+
+            }
+            System.out.println();
+        }
 
     }
 }
